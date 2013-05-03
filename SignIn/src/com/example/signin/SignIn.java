035dc58a38854a13889ef4c65c37d14505c42349
Cao The Hoang <thehoang.cao@gmail.com>
2013-05-03 12:38:22 +0700

    Dang nhap

---
+++ SignIn/src/com/example/signin/SignIn.java
@@ -0,0 +1,12 @@
+package com.example.signin;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class SignIn extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		}
+}
